ed411067b7da84fd396f4ccc3dab1edb345173df
Serge TALAKI <sergestalaki@gmail.com>
2020-09-09 17:48:16 +0000

    Update decimal.js
    Add option to transform non numbers' char to float ex number("Hello")= 0;
    
    number("124563.00745", 2) = 124563.00 = 124563 according  to precision and numbers after the float

--- decimal.js
+++ decimal.js
@@ -1,3 +1,3 @@
 function number(nb, precision){
-    return "".concat(nb).split(".").map((piece, index)=>(index > 0)? Math.round(parseFloat("0.".concat(piece))*Math.pow(10,precision||2)):piece).join(".");
+    return parseFloat("".concat(nb).split(".").map((piece, index)=>(index > 0)? Math.round(parseFloat("0.".concat(piece))*Math.pow(10,precision||2)):piece).join(".")) || 0;
 }
